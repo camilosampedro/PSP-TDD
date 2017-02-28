d528a19414c5651cd3bd90f92f96d839532eafd6
Camilo Sampedro <camilo.sampedro@udea.edu.co>
2017-02-27 20:01:26 -0500

    Creado punto de partida PSP-1

---
+++ PSP1/src/main/java/co/edu/udea/App.java
@@ -0,0 +1,13 @@
+package co.edu.udea;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
