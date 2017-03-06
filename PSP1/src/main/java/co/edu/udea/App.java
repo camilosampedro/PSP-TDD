7d6c047b831f53a7c19340d13b87063a8e99254f
Camilo Sampedro <camilo.sampedro@udea.edu.co>
2017-03-05 20:26:43 -0500

    Eliminados directorios .idea

--- PSP1/src/main/java/co/edu/udea/App.java
+++ PSP1/src/main/java/co/edu/udea/App.java
@@ -2,12 +2,9 @@ package co.edu.udea;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
