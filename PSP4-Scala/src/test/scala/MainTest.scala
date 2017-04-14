844cbb587b14a0f68f6f47f404a6067b4fb7b03a
Camilo Sampedro <camilo.sampedro@udea.edu.co>
2017-04-13 23:37:41 -0500

    Main test fixed

--- PSP4-Scala/src/test/scala/MainTest.scala
+++ PSP4-Scala/src/test/scala/MainTest.scala
@@ -9,7 +9,7 @@ class MainTest extends WordSpec with MockitoSugar {
   private def theParameters = afterWord("the parameters")
 
   val noArguments = Array.empty[String]
-  val expectedArguments = Array(getClass.getResource("psp3.txt").getFile, "14")
+  val expectedArguments = Array("9", "1.1", "0.00001")
 
   "The main execution" when theParameters {
     "are empty" should {
